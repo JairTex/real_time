0a8b6d0a859c0d6812c9e88a052526baf341986f
JairTex <steixeirajair@gmail.com>
2022-08-07 15:44:53 -0300

    Criação de consumers.py

---
+++ chat/consumers.py
@@ -0,0 +1,44 @@
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+import json
+
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['nome_sala'] #Recuperando nome da sala
+        self.room_group_name = f'chat_{self.room_name}' #retornando isso como nome da sala
+
+        #Entrando na sala
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.room_name
+        )
+        await self.accept()
+
+    async def disconnect(self, code):
+        #Saindo da sala da sala
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.room_name
+        )
+    
+    #Recebendo mensagem do webSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        mensagem = text_data_json['mensagem']
+
+        #Enviando mensagem para a sala
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type' : 'chat_message',
+                'message' : mensagem
+            }
+        )
+
+    #Recebendo mensagem da sala
+    async def chat_message(self, event):
+        mensagem = event['message']
+
+        #Enviando a mensagem para o WebSocket
+        await self.send(text_data=json.dumps({
+            'mensagem' : mensagem
+        }))
